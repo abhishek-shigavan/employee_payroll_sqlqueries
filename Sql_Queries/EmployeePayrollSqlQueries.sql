69ab8a6c5cb1e6b7761712a23b47e2980977a785
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-07-15 21:42:00 +0530

    Refactor: Creating employee_payroll table

--- Sql_Queries/EmployeePayrollSqlQueries.sql
+++ Sql_Queries/EmployeePayrollSqlQueries.sql
@@ -1,4 +1,16 @@
 # UC1
-create database payroll_service_db;
-show databases;
-use payroll_service_db;
+create database payroll_service_db;		# create database
+show databases;					# To see all databases
+use payroll_service_db;				# To use payroll_service_db database
+
+# UC2
+create table employee_payroll 					# creating employee_payroll table 
+(
+    Id int unsigned not null auto_increment,			# Id of employee
+    EmpName varchar(120) not null,				# Name of employee
+    Salary double not null,					# Salary of employee
+    StartDate date not null,					# Start date of employess
+    primary key(Id)						# make Id primary key 
+);
+show tables;							# show all tables of database
+describe employee_payroll;					# show structure of employee_payroll table
